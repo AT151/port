bf1068a4f270b88b8065955dc7120fab11979a8c
Anh Tran <atran25@LAPTOP-GTL047AB.localdomain>
2020-01-31 09:15:02 -0500

    Updates

---
+++ precache-manifest.a5346810e15792f3169926f2fca1219d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f15ecef425d75bf74a8d6c344e537df",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "43514fba3267e4decca3",
+    "url": "/port/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0ba334f729425ec4a34a",
+    "url": "/port/static/js/2.624e1675.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/port/static/js/2.624e1675.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43514fba3267e4decca3",
+    "url": "/port/static/js/main.e90ef3af.chunk.js"
+  },
+  {
+    "revision": "cc0d15626a0d2db3a12b",
+    "url": "/port/static/js/runtime-main.b839eec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/port/static/media/logo.5d5d9eef.svg"
+  }
+]);
